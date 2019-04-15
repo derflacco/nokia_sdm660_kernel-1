6f26c1d1f5571f6d0a1702495d6c82ac4bf64acd
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 17:42:31 -0700

    ASoC: msm: qdsp6v2: Fix compile errors when debugfs is disabled
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- drivers/misc/qcom/qdsp6v2/audio_alac.c
+++ drivers/misc/qcom/qdsp6v2/audio_alac.c
@@ -275,7 +275,6 @@ static int audio_open(struct inode *inode, struct file *file)
 	char name[sizeof "msm_alac_" + 5];
 #endif
 	audio = kzalloc(sizeof(struct q6audio_aio), GFP_KERNEL);
-
 	if (!audio) {
 		pr_err("Could not allocate memory for alac decode driver\n");
 		return -ENOMEM;
@@ -344,6 +343,8 @@ static int audio_open(struct inode *inode, struct file *file)
 
 	if (IS_ERR_OR_NULL(audio->dentry))
 		pr_debug("debugfs_create_file failed\n");
+#endif
+
 	pr_debug("%s:alacdec success mode[%d]session[%d]\n", __func__,
 						audio->feedback,
 						audio->ac->session);
--- drivers/misc/qcom/qdsp6v2/audio_g711alaw.c
+++ drivers/misc/qcom/qdsp6v2/audio_g711alaw.c
@@ -296,6 +296,7 @@ static int audio_open(struct inode *inode, struct file *file)
 
 	if (IS_ERR_OR_NULL(audio->dentry))
 		pr_debug("%s: debugfs_create_file failed\n", __func__);
+#endif
 	pr_debug("%s: g711dec success mode[%d]session[%d]\n", __func__,
 						audio->feedback,
 						audio->ac->session);
