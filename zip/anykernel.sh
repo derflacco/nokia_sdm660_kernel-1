530a38ba9b3e8cacc3c4a56ebae4c2f4ba8dcfef
derflacco <paul89rulez@gmail.com>
2019-06-24 00:25:39 +0200

    update anykernel.sh

--- zip/anykernel.sh
+++ zip/anykernel.sh
@@ -9,16 +9,16 @@ do.devicecheck=0
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=platina
-device.name2=platina-user
-device.name3=MI 8 Lite
+device.name1
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -35,13 +35,18 @@ dump_boot;
 
 # begin ramdisk changes
 
+# Add skip_override parameter to cmdline so user doesn't have to reflash Magisk
+if [ -d $ramdisk/.subackup -o -d $ramdisk/.backup ]; then
+  ui_print " "; ui_print "Magisk detected! Patching cmdline so reflashing Magisk is not necessary...";
+  patch_cmdline "skip_override" "skip_override";
+else
+  patch_cmdline "skip_override" "";
+fi;
+
 # Remove CAF Boost Framework cuz CAF is a hoe
 mount -o rw,remount -t auto /vendor >/dev/null; 
 rm -rf /vendor/etc/perf;
 mount -o ro,remount -t auto /vendor >/dev/null;
-
-# init.rc
-insert_line init.rc 'ancient' after 'import /init.\${ro.zygote}.rc' 'import /init.ancient.rc';
 
 # If the kernel image and dtbs are separated in the zip
 #decompressed_image=/tmp/anykernel/kernel/Image
@@ -59,24 +64,7 @@ insert_line init.rc 'ancient' after 'import /init.\${ro.zygote}.rc' 'import /ini
 #  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
 #fi;
 
-# Patch F2FS thanks Aradium
-vft=/vendor/etc/fstab.qcom;
-$bb mount -o rw,remount -t auto /system >/dev/null;
-$bb mount -o rw,remount -t auto /vendor 2>/dev/null;
-if [ -n $(grep "f2fs" $vft 2>/dev/null) != "" ]; then
-  ui_print "";
-  ui_print "";
-  ui_print "Patching F2FS...";
-backup_file $vft;
-insert_line $vft 'f2fs-data' after '/dev/block/bootdevice/by-name/userdata' '/dev/block/bootdevice/by-name/userdata   /data                  f2fs   noatime,nosuid,nodev,nodiratime,fsync_mode=nobarrier,background_gc=off   wait,check,encryptable=footer,crashcheck,quota,reservedsize=128M';
-insert_line $vft 'f2fs-cache' after '/dev/block/bootdevice/by-name/cache' '/dev/block/bootdevice/by-name/cache      /cache                 f2fs   noatime,nosuid,nodev,nodiratime,discard,fsync_mode=nobarrier,inline_xattr,inline_data,data_flush   wait';
-else
-  ui_print "";
-  ui_print "";
-  ui_print "Your ROM already support F2FS partitions";
-fi;
-$bb mount -o ro,remount -t auto /system >/dev/null;
-$bb mount -o ro,remount -t auto /vendor 2>/dev/null;
+
 
 # end ramdisk changes
 
